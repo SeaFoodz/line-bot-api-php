7565555ef6e87af48c89fa69c4b9ac3ecf661696
SeaFoodz <57056640+SeaFoodz@users.noreply.github.com>
2019-10-27 14:15:17 +0700

    Update netpie.php

--- netpie.php
+++ netpie.php
@@ -1,8 +1,8 @@
  <?php
 	function pubMqtt($topic,$msg){
-		$APPID= "LineBotLED/"; //enter your appid
-		$KEY = "RY1yuFxqaJOqqGg"; //enter your web key
-		$SECRET = "8iAcnJfGMQeZ0fom44Vk9LzOp"; //enter your secret
+		$APPID= "TeerapatIoT/"; //enter your appid
+		$KEY = "kmFoOyJwAcSygC7"; //enter your web key
+		$SECRET = "B2PuWBqva4ffgrFtqHu7vXXex"; //enter your secret
 		$Topic = "$topic";
 		if($msg =="on"){
 			$msg = "1";
