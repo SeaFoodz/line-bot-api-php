7eec206c5ce427795a0e6fdde413ace19a1b5ab3
SeaFoodz <57056640+SeaFoodz@users.noreply.github.com>
2019-10-27 14:20:22 +0700

    Update line.php

--- line.php
+++ line.php
@@ -1,7 +1,8 @@
 <?php
 
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
-$ACCESS_TOKEN = 'Vrt2nU13R4ucf2xg6WN5n4Z9+qO4ADuB6GcruFri1Ix3I8gb6MKLBRhQNjgPgJfhssqJcbco3DBJlfQFiP6ryEBlN2ufr552CJyYJWIwdo2fFEfL0TB/mYpTebxOsmwr4EvpUjRpdYAws1WpD2fbOgdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$ACCESS_TOKEN = 'Hp2jQOR7m1zC6jYRdCcCtRz9HOQpov9Q2lCAIiPnMEzl6oZp2GaOPo5/67lFzaEpfoyn7H8L5n/eUFZIDDk/wDcrLy2wRJW3xEnjxP9K+hDyVqxDMV3VszIW9hJfOWhJaQq1UwH7200QeqFj93Vb5QdB04t89/1O/w1cDnyilFU=
+'; // Access Token ค่าที่เราสร้างขึ้น
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
 $request = file_get_contents('php://input');   // Get request content
